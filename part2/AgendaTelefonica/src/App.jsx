b8f80d81602637d00d32bd8740c0aabe367109a1
rubendesrod <rubendr@alumnos.iesgalileo.es>
2024-08-02 16:08:38 +0200

    Recat part2 apartado-d

--- part2/AgendaTelefonica/src/App.jsx
+++ part2/AgendaTelefonica/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import personService from "./services/persons";
 import "./App.css";
 
 // Componente para realizar la búsqueda
@@ -33,27 +33,26 @@ const FormData = (props) => {
 };
 
 // Compoenente para cada persona que se registre en la agenda
-const Persons = ({ listPersons }) => {
+const Persons = ({ listPersons, deletePerson }) => {
   return (
     <ul>
       {listPersons.map((p) => (
         <li key={p.id}>
-          {p.name} | {p.number}
+          {p.name} | {p.number}{" "}
+          <button onClick={() => deletePerson(p.id)}>delete</button>
         </li>
       ))}
     </ul>
   );
 };
+
 // Componte principal
 function App() {
   // Estado con un array de objetos de personas
   const [persons, setPersons] = useState([]);
 
-  // Hago la llamada al servidor para recibir las personas del DB.JSON
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => setPersons(response.data));
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
   }, []);
 
   // Estado para el nombre de la persona
@@ -70,26 +69,64 @@ function App() {
     return persons.some((person) => person.name === name);
   };
 
+  // Método para actualizar el telefono de una persona
+  const aupdatePerson = () => {
+    const msg = `${newName} existe actualmente en la agenda quieres actualizar su numero de telefono?`;
+    // Se pregunta al usuario si quiere que se actualize el numero de telefono de la persona que ya existe
+    if (window.confirm(msg)) {
+      const person = persons.find((p) => p.name === newName);
+      const updatedPerson = { ...person, number: newPhone };
+
+      personService
+        .update(person.id, updatedPerson)
+        .then((returnedPerson) => {
+          setPersons(
+            persons.map((p) => (p.id !== person.id ? p : returnedPerson))
+          );
+          setNewName("");
+          setNewPhone("");
+        })
+        .cath((e) => {
+          alert(`${person.name} no se ha podido actualizar`);
+        });
+    } else {
+      setNewName("");
+      setNewPhone("");
+    }
+  };
+
+  // Metodo para borrar a una persona
+  const deletePerson = (id) => {
+    if (window.confirm(`Quieres borrar esta persona de la agenda?`)) {
+      personService.updown(id).then((personDelete) => {
+        alert(`${personDelete.name} ha sido borrado de la agenda`);
+        const personsModified = persons.filter((p) => p.id !== id);
+        console.log(personsModified);
+        setPersons(personsModified);
+      });
+    }
+  };
+
   // Funcion para añadir una persona
   const addPerson = (event) => {
     event.preventDefault();
-    setNewName("");
-    setNewPhone("");
 
     if (comprobarNombre(newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
+      aupdatePerson()
+      return
     }
 
     // Creo un objeto nuevo para concatenar con el array de objetos
     const objectPerson = {
       name: newName,
       number: newPhone,
-      id: persons.length + 1,
     };
 
-    // No se utiliza el mismo array del estado al usar concat se realiza una copia de este
-    setPersons(persons.concat(objectPerson));
+    personService.create(objectPerson).then((returnedPersona) => {
+      setPersons(persons.concat(returnedPersona));
+      setNewName("");
+      setNewPhone("");
+    });
   };
 
   // Funcion para cuando el usuario ingrese un nombre
@@ -130,7 +167,7 @@ function App() {
 
       <h2>Numbers</h2>
 
-      <Persons listPersons={filteredPersons} />
+      <Persons listPersons={filteredPersons} deletePerson={deletePerson} />
     </div>
   );
 }
